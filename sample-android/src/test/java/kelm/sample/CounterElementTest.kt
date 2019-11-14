a222a1d77d94b33142454366e24163f047770869
Allan Yoshio Hasegawa <hasegawa.aran@gmail.com>
2019-11-14 22:43:42 +0000

    Tweaked CounterElementTest

--- sample-android/src/test/java/kelm/sample/CounterElementTest.kt
+++ sample-android/src/test/java/kelm/sample/CounterElementTest.kt
@@ -1,63 +1,60 @@
 package kelm.sample
 
 import io.kotlintest.shouldBe
+import kelm.sample.CounterElement.Model
 import kelm.sample.CounterElement.Msg
 import org.spekframework.spek2.Spek
 
 object CounterElementTest : Spek({
     group("given no user inputs") {
-        val msgs = msgs()
+        model {
+            test("counter should be zero") {
+                count shouldBe 0
+            }
 
-        test("counter should be zero") {
-            model(msgs).count shouldBe 0
-        }
-
-        test("the reset and minus buttons should be disabled") {
-            model(msgs).apply {
+            test("the reset and minus buttons should be disabled") {
                 resetBtEnabled shouldBe false
                 minusBtEnabled shouldBe false
             }
+
         }
     }
 
     group("given user clicked plus 3 times") {
-        val msgs = msgs(Msg.PlusClick, Msg.PlusClick, Msg.PlusClick)
+        model(Msg.PlusClick, Msg.PlusClick, Msg.PlusClick) {
+            test("counter should be 3") {
+                count shouldBe 3
+            }
 
-        test("counter should be 3") {
-            model(msgs).count shouldBe 3
-        }
-
-        test("the reset and minus buttons should be enabled") {
-            model(msgs).apply {
+            test("the reset and minus buttons should be enabled") {
                 resetBtEnabled shouldBe true
                 minusBtEnabled shouldBe true
             }
         }
+
     }
 
     group("given user clicked plus 3 times and minus 2 times") {
-        val msgs = msgs(
+        model(
             Msg.PlusClick, Msg.PlusClick, Msg.PlusClick,
             Msg.MinusClick, Msg.MinusClick
-        )
-
-        test("counter should be 1") {
-            model(msgs).count shouldBe 1
+        ) {
+            test("counter should be 1") {
+                count shouldBe 1
+            }
         }
     }
 
     group("given user clicked plus 3 times and reset one time") {
-        val msgs = msgs(
+        model(
             Msg.PlusClick, Msg.PlusClick, Msg.PlusClick,
             Msg.ResetClick
-        )
+        ) {
+            test("counter should be 0") {
+                count shouldBe 0
+            }
 
-        test("counter should be 0") {
-            model(msgs).count shouldBe 0
-        }
-
-        test("the reset and minus buttons should be disabled") {
-            model(msgs).apply {
+            test("the reset and minus buttons should be disabled") {
                 resetBtEnabled shouldBe false
                 minusBtEnabled shouldBe false
             }
@@ -65,15 +62,14 @@ object CounterElementTest : Spek({
     }
 })
 
-private fun msgs(vararg msgs: Msg) = msgs.toList()
-
-private fun model(msgs: List<Msg>) =
+private fun model(vararg msgs: Msg, testF: Model.() -> Unit) =
     CounterElement.test(CounterElement.initModel())
         .apply {
             if (msgs.isNotEmpty()) {
-                step(*msgs.toTypedArray())
+                step(*msgs)
             }
         }
         .let {
             it.history.last { it.modelPrime != null }.modelPrime!!
         }
+        .let(testF::invoke)
